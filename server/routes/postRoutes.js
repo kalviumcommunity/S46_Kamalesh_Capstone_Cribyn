f9d22bbf0bdb507ae025245cc05b6540bb1f89eb
k-46 <kamalxsh31@gmail.com>
2024-09-25 00:00:16 +0530

    add jwt token

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const postMethods = require("../controllers/postController.js");
+const { TokenAuthenticator } = require("../middlewares/UserAuthentication.js");
 
 // To access All the POSTS ✔️
-router.get("/all", postMethods.fetchAllData);
+router.get("/all", TokenAuthenticator, postMethods.fetchAllData);
 
 // To create a new POST ✔️
-router.post("/new", postMethods.createPost);
+router.post("/new", TokenAuthenticator, postMethods.createPost);
 
 // To Edit the Post   ✔️
-router.put("/:postid", postMethods.editPost);
+router.put("/:postid", TokenAuthenticator, postMethods.editPost);
 
 module.exports = router ;
